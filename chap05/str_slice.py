77dcde049c3c1bdef07347fb34dadc66e895934b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-01-06 22:18:58 +0900

    リスト5.8まで学習完了

--- chap05/str_slice.py
+++ chap05/str_slice.py
@@ -2,3 +2,10 @@ title = 'あいうえおかきくけこ'
 
 print(title[2])
 print(title[2:5])
+print(title[2:])
+print(title[:5])
+print(title[:])
+print(title[-7:])
+print(title[::2])
+print(title[1::2])
+print(title[::-1])
