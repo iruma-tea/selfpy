c6e256d5af9de0b149dfeceb658cb7815b07c063
iruma-tea <shusuke.think.pad@gmail.com>
2024-01-26 23:54:58 +0900

    8.4 関数呼び出しと戻り値の学習完了 chap08の学習完了

---
+++ chap08/args_higher.py
@@ -0,0 +1,16 @@
+# 高級関数walk_list関数
+def walk_list(data, func):
+    # リストの内容を順に処理
+    for key, value in enumerate(data):
+        # func経由で指定の関数を呼び出し
+        func(value, key)
+
+# リストを処理するためのユーザー定義関数
+
+
+def show_item(value, key):
+    print(key, ':', value)
+
+
+data = [105, 53, 27, 87, 33]
+walk_list(data, show_item)
---
+++ chap08/args_keyword_strict.py
@@ -0,0 +1,7 @@
+# arg2, 3はキーワード引数であること
+def my_func(arg1, *, arg2=0, arg3=0):
+    pass
+
+
+# my_func(1, 2, 3)
+my_func(1, arg2=2, arg3=3)
